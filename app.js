eb1663b1dc56244d0db025a654c7d84bae6e662e
Gelu Ungur <ImGelu@gmail.com>
2021-05-04 23:18:52 +0300

    Design Updates + Commented back-end code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Server Side
+
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
@@ -13,41 +15,49 @@ const io = socket(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Detects a signal from the Client when a new socket connection has been made
 io.on("connection", (socket) => {
+  // Detects a signal from the Client when a new user has joined the room
   socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
-
     socket.join(user.room);
 
-    // Sends a message to everyone
-    socket.emit("message", formatMessage("Bot", "Welcome!"));
+    // Sends a message to the current user
+    socket.emit("message", formatMessage("Bot", `You've joined the <strong>${user.room}</strong> room. Welcome!`)); // .emit() sends a message to the current user
 
-    // Broadcast when an user connects
-    socket.broadcast.to(user.room).emit("message", formatMessage("Bot", `${user.username} has joined the room!`)); // to all clients in the current namespace except the sender
+    // Sends a message to everyone in the current room when someone enters it
+    socket.broadcast.to(user.room).emit("message", formatMessage("Bot", `<strong>${user.username}</strong> has joined the room!`)); // .broadcast() sends a message to all clients in the current namespace except the sender
 
+    // Sends a signal to the Client to handle the users room list
     io.to(user.room).emit("roomUsers", {
       room: user.room,
       users: getRoomUsers(user.room),
     });
   });
 
-  // Listen for chatMessage
+  // Detects a signal from the Client when a new chat message has been sent
   socket.on("chatMessage", (msg) => {
     const user = getCurrentUser(socket.id);
 
+    // Sends a signal to the Client to handle the new sent message
     io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
-  // Runs when a client disconnects
+  // Detects a signal from the Client when an user's socket connection is lost
   socket.on("disconnect", () => {
     const user = userLeave(socket.id);
 
     if (user) {
-      io.to(user.room).emit("message", formatMessage("Bot", `${user.username} has left the chat!`));
+      // Sends a signal to the Client to handle the departure of the user
+      io.to(user.room).emit("userLeave");
+
+      // Sends a signal to the Client to send a message to everyone in the room
+      io.to(user.room).emit("message", formatMessage("Bot", `<strong>${user.username}</strong> has left the room!`));
     }
   });
 });
 
+// Checking if the Server is running correctly
 server.listen(port, () => {
   console.log(`Server running on port ${port}!`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,43 @@
+// Client Side
+
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
 const chatRoomName = document.getElementById("room-name");
 const chatRoomUsers = document.getElementById("room-users");
+const hamburgerBtn = document.getElementById("btn-sidebar");
+const chatSidebar = document.getElementById("chat-sidebar");
 
+// Getting username and room parameters from URL query
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
 const socket = io();
 
-// Join chatroom
+// Whenever an user opens a new socket connection, the Client sends a signal to the Server that an user has joined the room
 socket.emit("joinRoom", { username, room });
 
+// Detects a signal from the Server when the users list needs to be loaded in the DOM
 socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputRoomUsers(users);
 });
 
-// Message from server
+// Detects a signal from the Server when an user has left the room
+socket.on("userLeave", () => {
+  removeUserFromRoom(socket.id);
+});
+
+// Detects a signal from the Server when a new message has been sent
 socket.on("message", (message) => {
   outputMessage(message);
 
   chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
+///////////////////////////////////////////////////
+
+// Send message button event listener
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -39,10 +53,9 @@ chatForm.addEventListener("submit", (e) => {
 
 // Output message to DOM
 function outputMessage(message) {
-  console.log(message);
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p><p class="text">${message.text}</p>`;
+  div.innerHTML = `<p class="meta">${message.username} <span class="float-right">${message.time}</span></p><p class="text">${message.text}</p>`;
 
   document.querySelector(".chat-messages").appendChild(div);
 }
@@ -54,5 +67,24 @@ function outputRoomName(room) {
 
 // Add room users to DOM
 function outputRoomUsers(users) {
-  chatRoomUsers.innerHTML = `${users.map((user) => `<li>${user.username}</li>`).join("")}`;
+  chatRoomUsers.innerHTML = `${users.map((user) => `<li id="room-user-${user.id}">${user.username}</li>`).join("")}`;
 }
+
+// Remove user from DOM list
+function removeUserFromRoom(id) {
+  const li = document.getElementById(`room-user-${id}`);
+  li.remove();
+}
+
+// Bootstrap Tooltip initialization
+var locationTooltip = document.getElementById("btn-location");
+var tooltip = new bootstrap.Tooltip(locationTooltip);
+
+// Hamburger menu behaviour
+hamburgerBtn.addEventListener("click", (e) => {
+  if (chatSidebar.style.display === "none") {
+    chatSidebar.style.display = "block";
+  } else {
+    chatSidebar.style.display = "none";
+  }
+});
