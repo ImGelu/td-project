5bf88a0af31c8ab83fc9178870395adad0f5e63e
Gelu Ungur <ImGelu@gmail.com>
2021-05-30 12:59:03 +0300

    Production DB Config

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,16 @@
+// Production
 module.exports = {
+  host: "server218.web-hosting.com",
+  user: "geluweia_chat",
+  password: "fbGuestAg6y8",
+  database: "geluweia_chat",
+  dialect: "mysql",
+};
+
+/*module.exports = {
   host: "localhost",
   user: "root",
   password: "",
   database: "chat",
   dialect: "mysql",
-};
+};*/
